e147f3220d3c5030acfdcfa1d82a0e87dd0baf33
Daniel Holmen <daniel.holmen@bouvet.no>
2019-09-19 09:47:25 +0200

    Trying string

--- src/main/java/no/bouvet/xp/lib/xml2json/JsonConverter.java
+++ src/main/java/no/bouvet/xp/lib/xml2json/JsonConverter.java
@@ -7,6 +7,12 @@ import org.json.XML;
 
 public class JsonConverter {
     public String toXml(final Map jsonObject) throws Exception {
+      System.out.println("Map");
+      return XML.toString(new JSONObject(jsonObject));
+    }
+
+    public String toXml(final String jsonString) throws Exception {
+      System.out.println(jsonString);
       return XML.toString(new JSONObject(jsonObject));
     }
 }
