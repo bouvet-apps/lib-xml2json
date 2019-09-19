070124a3fa3b23df846873e4a997acbfd3c702de
Daniel Holmen <daniel.holmen@bouvet.no>
2019-09-19 09:32:23 +0200

    Debug

--- src/main/java/no/bouvet/xp/lib/xml2json/JsonConverter.java
+++ src/main/java/no/bouvet/xp/lib/xml2json/JsonConverter.java
@@ -5,6 +5,7 @@ import org.json.XML;
 
 public class JsonConverter {
     public String toXml(final String jsonObject) throws Exception {
+      System.out.println(jsonObject);
         return XML.toString(new JSONObject(jsonObject));
     }
 }
